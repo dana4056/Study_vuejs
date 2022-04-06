43805e2a21394629074ac152d8fb34434ff033e5
dana <dana4056@naver.com>
2022-04-06 18:50:30 +0900

    뷰 프로젝트, 컴포넌트 생성

---
+++ d_day_vue/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <DdayHeader></DdayHeader>
+    <DdayInput></DdayInput>
+    <DdayList></DdayList>
+    <DdayFooter></DdayFooter> 
+  </div> 
+</template>
+
+<script>
+import DdayHeader from './components/DdayHeader.vue'
+import DdayInput from './components/DdayInput.vue'
+import DdayList from './components/DdayList.vue'
+import DdayFooter from './components/DdayFooter.vue'
+
+export default {
+  components: {
+    'DdayHeader': DdayHeader,
+    'DdayInput': DdayInput,
+    'DdayList': DdayList,
+    'DdayFooter': DdayFooter
+  }
+}
+</script>
+
+<style>
+body{
+  text-align: center;
+}
+</style>
