bd506c7e318e245be3a18af40dbcb49db104e1c0
dana <dana4056@naver.com>
2022-04-06 17:18:08 +0900

    컴포넌트 별 기능 구현 및 디자인

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,16 +1,62 @@
 <template>
-<!-- 컴포넌트 당 template의 root 태그(root 아래태그?)는 단 하나 -->
-  <div>
-      input
+  <div class="inputBox shadow">  <!-- 컴포넌트 당 template의 root 태그는 단 하나 -->
+      <!-- v-model은 화면(input값)값과 스크립트(인스턴스)값을 동기화 시켜줌  -->
+      <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo">  
+      <!-- <button v-on:click="addTodo">add</button> -->
+      <span class="addContainer">
+        <i class="fa-solid fa-plus addBtn" v-on:click="addTodo"></i>
+      </span>
   </div>
 </template>
 
 <script>
 export default {
-
+  data: function(){
+    return {
+      newTodoItem:""
+    }
+  }, 
+  methods: {
+    addTodo: function(){
+      if(this.newTodoItem !== ""){ // 입력값이 있을 때만!
+        var obj = {completed: false, item: this.newTodoItem}
+        // 저장하는 로직 실행(JS window객체의 localStorage사용)
+        // JSON.stringfy(객체): 객체를 str으로 변환
+        localStorage.setItem(this.newTodoItem, JSON.stringify(obj));   //값 저장
+        this.clearInput();
+      }
+    },
+    clearInput: function(){
+      this.newTodoItem='';  // input 초기화
+    }
+  }
 }
 </script>
 
-<style>
+<style scoped>
+input:focus{
+  outline:none;
+}
+.inputBox{
+  background:white;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
+}
+.inputBox input {
+  border-style: none;
+  font-size: 0.9rem;
+}
+.addContainer{
+  float: right;
+  background: linear-gradient(to right, #6478Fb, #8763FB);
+  display:block;
+  width:3rem;
+  border-radius: 0 5px 5px 0;
+}
+.addBtn{
+  color:white;
+  vertical-align: middle;
+}
 
 </style>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,15 +1,79 @@
 <template>
   <div>
-      list
+      <ul>
+        <li v-for="(todoItem, index) in todoItems" v-bind:key="todoItem.item" class="shadow">
+          <i class="checkBtn fa-solid fa-check" v-bind:class="{checkBtnCompleted: todoItem.completed}" v-on:click="toggleComplete(todoItem, index)"></i>
+          <span v-bind:class="{textCompleted: todoItem.completed}">{{ todoItem.item }}</span>
+          <span class="removeBtn" v-on:click="removeTodo(todoItem, index)"><i class="fa-solid fa-trash-can"></i></span>
+        </li>
+      </ul>
   </div>
 </template>
 
 <script>
 export default {
+  data: function(){
+    return {
+      todoItems:[]
+    }
+  },
+  methods: {
+    removeTodo: function(todoItem, index){
+      localStorage.removeItem(todoItem.item);
+      this.todoItems.splice(index, 1);  //JS배열 문법 지우고 새로운 배열 반환
+    }, 
+    toggleComplete: function(todoItem){
+      todoItem.completed = !todoItem.completed ;
 
+      // 로컬 스토리지 갱신: 로컬 스토리지 업데이트 API 없어서 삭제하고 다시 넣어줌
+      localStorage.removeItem(todoItem.item);
+      localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
+    }
+  },
+  created: function(){
+    if (localStorage.length > 0){ // 로컬 스토리지에 데이터가 존재한다면
+      for( var i = 0; i < localStorage.length; i++){
+        if(localStorage.key(i)!=='loglevel:webpack-dev-server'){
+          this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));  //값이 객체임{comp: false, item:"todos"}
+          // this.todoItems.push(localStorage.key(i));  //키와 value가 같았을때 쓴 방법
+        }
+      }
+    }
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+ul{
+  list-style-type: none; /* 리스트에서 점없애기 */ 
+  padding-left: 0px;
+  margin-top: 0;
+  text-align: left;
+}
+li{
+  display: flex;
+  min-height: 50px;
+  height: 50px;
+  line-height: 50px;
+  margin: 0.5rem 0;
+  padding: 0 0.9rem;
+  background: white;
+  border-radius: 5px;
+}
+.removeBtn{
+  margin-left: auto;
+  color: #de4343;
+}
+.checkBtn{
+  line-height: 45px;
+  color: #62acde;
+  margin-right:5px;
+}
+.checkBtnCompleted{
+  color: #b3adad;
+}
+.textCompleted{
+  text-decoration: line-through;
+  color: #b3adad;
+}
 </style>
