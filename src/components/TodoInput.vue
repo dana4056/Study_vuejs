4a69d247e54c763692ae0de1aa8951aad2003855
dana <dana4056@naver.com>
2022-04-08 16:39:18 +0900

    모달컴포넌트 생성

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,19 +1,31 @@
 <template>
   <div class="inputBox shadow">  <!-- 컴포넌트 당 template의 root 태그는 단 하나 -->
-      <!-- v-model은 화면(input값)값과 스크립트(인스턴스)값을 동기화 시켜줌  -->
-      <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo">  
-      <!-- <button v-on:click="addTodo">add</button> -->
-      <span class="addContainer">
-        <i class="fa-solid fa-plus addBtn" v-on:click="addTodo"></i>
-      </span>
+    <!-- v-model은 화면(input값)값과 스크립트(인스턴스)값을 동기화 시켜줌  -->
+    <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo">  
+    <!-- <button v-on:click="addTodo">add</button> -->
+    <span class="addContainer">
+      <i class="fa-solid fa-plus addBtn" v-on:click="addTodo"></i>
+    </span>
+
+    <!------------------ 모달 컴포넌트 사용 ---------------------------->
+    <ModalModal :show="showModal" @close="showModal = false">
+      <template #header>
+        <h3>custom header</h3>
+      </template>
+    </ModalModal>
+    <!----------------------------------------------------------------->
+
   </div>
 </template>
 
 <script>
+import ModalModal from './common/ModalModal.vue'
+
 export default {
   data: function(){
     return {
-      newTodoItem:""
+      newTodoItem:"",
+      showModal:false
     }
   }, 
   methods: {
@@ -22,10 +34,16 @@ export default {
         this.$emit("addTodoItem",this.newTodoItem);
         this.clearInput();
       }
+      // else{
+        
+      // }
     },
     clearInput: function(){
       this.newTodoItem='';  // input 초기화
     }
+  },
+  components:{
+    'ModalModal': ModalModal
   }
 }
 </script>
