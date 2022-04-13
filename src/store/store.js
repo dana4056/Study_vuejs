ff193dc3ec4c71e7b28543bcfa5d8690781761ee
dana <dana4056@naver.com>
2022-04-13 14:56:22 +0900

    구조화: 기능별로

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,10 @@
 // import Vue from 'vue' 
 import Vuex from 'vuex'
 
+// 해당 파일에서 export된 모든 것을 가져와서 as 뒤(getters,mutations)에 담기
+import * as getters from './getters'  
+import * as mutations from './mutations'
+
 // Vue.use(Vuex); //plugin
 
 const storage = {
@@ -21,31 +25,7 @@ export const store = new Vuex.Store({
     state:{
         todoItems:storage.fetch()
     },
-    getters: {
-        storedTodoItmes(state){
-            return state.todoItems;
-        }
-    },
-    mutations:{
-        addOneItem(state, todoItem){
-            const obj = {completed: false, item: todoItem};
-            localStorage.setItem(todoItem, JSON.stringify(obj));  
-            //this. 말고 state.로 접근
-            state.todoItems.push(obj); 
-        },
-        removeOneItem(state, payload){
-            localStorage.removeItem(payload.todoItem.item);
-            state.todoItems.splice(payload.index, 1); 
-        },
-        toggleOneItem(state, payload){
-            state.todoItems[payload.index].completed = !state.todoItems[payload.index].completed; 
-            localStorage.removeItem(payload.todoItem.item);
-            localStorage.setItem(payload.todoItem.item, JSON.stringify(payload.todoItem));
-        },
-        clearAllItems(state){
-            localStorage.clear(); 
-            state.todoItems = [];
-        }
-    }
+    getters,
+    mutations 
 });
 
