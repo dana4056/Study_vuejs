01c0ccf37806325f4c9e6f813b50e28e012f74c5
dana <dana4056@naver.com>
2022-05-04 17:44:18 +0900

    Store 속성 모듈화 (actions, mutations)

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vuex from 'vuex'
-import { fetchNewsList, fetchJobsList, fetchAskList } from '../api/index.js'
+import mutations from './mutations.js'
+import actions from './actions.js'
 
 export const store = new Vuex.Store({
     state: {
@@ -12,44 +13,6 @@ export const store = new Vuex.Store({
             return state.ask;
         }
     },
-    mutations:{
-        SET_NEWS(state, news){
-            state.news = news;
-        },
-        SET_JOBS(state, jobs){
-            state.jobs = jobs;
-        },
-        SET_ASK(state,ask){
-            state.ask = ask;
-        }
-    },
-    actions: {
-        FETCH_NEWS(context){
-            fetchNewsList()
-            .then(response =>{
-                // 아래 문법 안됨(actions에서 바로 states에 접근x)
-                // this.state.new = response.data;   
-                // mutations 이용해야함
-                context.commit('SET_NEWS', response.data);
-            })
-            .catch(error =>{
-                console.log(error);
-            })
-        },
-        FETCH_JOBS({commit}){
-            fetchJobsList()
-            .then(({ data }) => {
-                //디스트럭처링 문법data랑 commit 바로 꺼냄
-                commit('SET_JOBS', data)
-            })
-            .catch(error => console.log(error))
-        },
-        FETCH_ASK({commit}){
-            fetchAskList()
-            .then(({data}) => {
-                commit('SET_ASK', data)
-            })
-            .catch(error => console.log(error))
-        }
-    }
+    mutations,
+    actions
 });
